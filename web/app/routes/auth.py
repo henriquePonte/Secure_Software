2461e6442b0e6136ac7702ef2bb07430f45ddd82
Henrique Ponte <107682582+henriquePonte@users.noreply.github.com>
2026-05-01 13:01:48 +0100

    auth logger closes #22

--- web/app/routes/auth.py
+++ web/app/routes/auth.py
@@ -1,14 +1,20 @@
 import flask
 from ..extensions import get_db
 from ..services.user import get_user_by_username
+from app.logger.logger import get_logger
 
 bp = flask.Blueprint("auth", __name__)
+
+logger = get_logger(__name__)
 
 
 @bp.route("/")
 def index():
+    user_id = flask.session.get("user_id")
     if flask.session.get("user_id"):
+        logger.info(f"Authenticated user_id={user_id} redirected to documents_page")
         return flask.redirect(flask.url_for("documents.documents_page"))
+    logger.info("Anonymous user redirected to login page")
     return flask.redirect(flask.url_for("auth.login"))
 
 
@@ -17,6 +23,8 @@ def login():
     if flask.request.method == "POST":
         username = flask.request.form.get("username", "")
         password = flask.request.form.get("password", "")
+
+        logger.info(f"Login attempt for username={username}")
 
         conn = get_db()
         cur = conn.cursor()
@@ -31,11 +39,16 @@ def login():
             flask.session["user_id"] = user[0]
             flask.session["username"] = username
 
+            logger.info(f"Login successful for user_id={user[0]}, username={username}")
+
             if username == "admin":
+                logger.info("Admin redirected to dashboard")
                 return flask.redirect(flask.url_for("admin.dashboard"))
             else:
+                logger.info("user redirected to documents_page")
                 return flask.redirect(flask.url_for("documents.documents_page"))
 
+        logger.warning(f"Login failed - invalid password or disabled user: {username}")
         flask.flash("Invalid credentials.", "error")
 
     return flask.render_template("login.html")
@@ -43,5 +56,7 @@ def login():
 
 @bp.route("/logout")
 def logout():
+    username = flask.session.get("username")
     flask.session.clear()
+    logger.info(f"User logged out: {username}")
     return flask.redirect(flask.url_for("auth.login"))
