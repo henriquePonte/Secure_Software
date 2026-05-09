f7ff99ec5fef144d6ce1bb891b4f347dc4b50970
Henrique Ponte <107682582+henriquePonte@users.noreply.github.com>
2026-05-09 18:07:58 +0100

    Update extensions.py

--- web/app/extensions.py
+++ web/app/extensions.py
@@ -1,15 +1,23 @@
 import os
+import time
 import psycopg2
 
 def get_db():
-    if os.getenv("DATABASE_URL"):
-        return psycopg2.connect(os.getenv("DATABASE_URL"))
+    database_url = os.getenv("DATABASE_URL")
 
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        dbname=os.getenv("DB_NAME"),
-        sslmode="require"
-    )
+    for _ in range(10):
+        try:
+            if database_url:
+                return psycopg2.connect(database_url)
+
+            return psycopg2.connect(
+                host=os.getenv("DB_HOST"),
+                port=os.getenv("DB_PORT"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                dbname=os.getenv("DB_NAME"),
+            )
+        except Exception:
+            time.sleep(1)
+
+    raise RuntimeError("Database not available")
