0daef11e849a044e831d1aba22977d4735cc2d28
Henrique Ponte <107682582+henriquePonte@users.noreply.github.com>
2026-05-09 17:56:34 +0100

    Update extensions.py

--- web/app/extensions.py
+++ web/app/extensions.py
@@ -1,11 +1,15 @@
-import psycopg2
 import os
+import psycopg2
 
 def get_db():
+    if os.getenv("DATABASE_URL"):
+        return psycopg2.connect(os.getenv("DATABASE_URL"))
+
     return psycopg2.connect(
-        host=os.getenv("DB_HOST", "db"),
-        port=os.getenv("DB_PORT", "5432"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD", "postgres"),
-        dbname=os.getenv("DB_NAME", "docdb"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        dbname=os.getenv("DB_NAME"),
+        sslmode="require"
     )
